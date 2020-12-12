1e3c946c14c154802cb6bea044d2b5f83147c016
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-12-11 22:08:02 -0800

    Update deployment settings

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 ############################################################
 # SETUP
 ############################################################
+load_dotenv()
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+MONGODB_DBNAME = 'mydb'
 
 app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://sanchezjairo:<password>@cluster0.di1hm.mongodb.net/<dbname>?retryWrites=true&w=majority")
+db = client.test
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
 mongo = PyMongo(app)
